75c8259465068e416c6cc5b311e487f585636c06
Muhammed Arafath <marafath368@gmail.com>
2022-10-18 11:41:53 +0530

    Client Initial 1

--- Restore/API/Controllers/ProductController.cs
+++ Restore/API/Controllers/ProductController.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/products")]
     public class ProductController : ControllerBase
     {
         private readonly StoreContext _context;
---
+++ Restore/client/src/app/layout/App.tsx
@@ -0,0 +1,32 @@
+import { ThemeProvider } from "@emotion/react";
+import { Container, createTheme, CssBaseline, Typography } from "@mui/material";
+import { useState } from "react";
+import Catalog from "../../features/catalog/catalog";
+import Header from "./header";
+
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container>
+        <Catalog />
+      </Container>
+    </ThemeProvider>
+  );
+}
+export default App;
